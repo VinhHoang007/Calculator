69d463a31aee59d0f978cf532d99b1d3c429111b
unknown <vinhhoangngoc22@gmail.com>
2021-02-01 18:25:18 +0700

    calculator

---
+++ myscript.js
@@ -0,0 +1,26 @@
+function Addition(){
+    let number1= +document.getElementById("number1").value;
+    let number2= +document.getElementById("number2").value;
+    let add = number1 + number2;
+    window.alert(add);
+}
+function Subtraction(){
+    let number1= +document.getElementById("number1").value;
+    let number2= +document.getElementById("number2").value;
+    let sub = number1 - number2;
+    window.alert(sub);
+
+}
+function Multiplication(){
+    let number1= +document.getElementById("number1").value;
+    let number2= +document.getElementById("number2").value;
+    let mul = number1 * number2;
+    window.alert(mul);
+
+}
+function Devision(){
+    let number1= +document.getElementById("number1").value;
+    let number2= +document.getElementById("number2").value;
+    let devide = number1 / number2;
+    window.alert(devide);
+}
